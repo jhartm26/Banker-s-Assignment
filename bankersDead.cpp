2e3a0fcf5ef708a10f50d76e3cda736922afca2d
jhartm26 <90359108+jhartm26@users.noreply.github.com>
2021-11-29 14:22:07 -0500

    File input working
    Not the greatest organization but it works

--- bankersDead.cpp
+++ bankersDead.cpp
@@ -1,133 +1,136 @@
-// C++ program to illustrate Banker's Algorithm
-#include<iostream>
+//C++ program to illustrate Banker's Algorithm
+//Operating Systems Programming Assignment #2
+//Jacob Hartmann
+//11/29/2021
+
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-// Number of processes
+//Number of processes
 const int P = 5;
-
-// Number of resources
+//Number of resources
 const int R = 3;
 
-// Function to find the need of each process
-void calculateNeed(int need[P][R], int maxm[P][R],
-				int allot[P][R])
-{
-	// Calculating Need of each P
-	for (int i = 0 ; i < P ; i++)
-		for (int j = 0 ; j < R ; j++)
-
-			// Need of instance = maxm instance -
-			//				 allocated instance
+//Function to find the need of each process
+void calculateNeed(int need[P][R], int maxm[P][R], int allot[P][R]) {
+	for (int i = 0 ; i < P ; i++) {
+		for (int j = 0 ; j < R ; j++) {
+			//Need of instance = maxm instance - allocated instance
 			need[i][j] = maxm[i][j] - allot[i][j];
+		}
+	}
 }
 
-// Function to find the system is in safe state or not
-bool isSafe(int processes[], int avail[], int maxm[][R],
-			int allot[][R])
-{
+//Function to find the system is in safe state or not
+bool isSafe(int processes[], int avail[], int maxm[][R], int allot[][R]) {
 	int need[P][R];
-
-	// Function to calculate need matrix
 	calculateNeed(need, maxm, allot);
 
-	// Mark all processes as infinish
+	//Mark all processes as infinish
 	bool finish[P] = {0};
-
-	// To store safe sequence
+	//To store safe sequence
 	int safeSeq[P];
 
-	// Make a copy of available resources
+	//Make a copy of available resources
 	int work[R];
-	for (int i = 0; i < R ; i++)
+	for (int i = 0; i < R ; i++){
 		work[i] = avail[i];
+	}
 
-	// While all processes are not finished
-	// or system is not in safe state.
+	//While all processes are not finished or system is not in safe state.
 	int count = 0;
-	while (count < P)
-	{
-		// Find a process which is not finish and
-		// whose needs can be satisfied with current
-		// work[] resources.
+	while (count < P) {
+		//Find a process which is not finish and whose needs can be satisfied with current work[] resources.
 		bool found = false;
-		for (int p = 0; p < P; p++)
-		{
-			// First check if a process is finished,
-			// if no, go for next condition
-			if (finish[p] == 0)
-			{
-				// Check if for all resources of
-				// current P need is less
-				// than work
+		for (int p = 0; p < P; p++) {
+			//First check if a process is finished, if no, go for next condition
+			if (finish[p] == 0) {
+				//Check if for all resources of current P need is less than work
 				int j;
-				for (j = 0; j < R; j++)
-					if (need[p][j] > work[j])
+				for (j = 0; j < R; j++) {
+					if (need[p][j] > work[j]) {
 						break;
-
-				// If all needs of p were satisfied.
-				if (j == R)
-				{
-					// Add the allocated resources of
-					// current P to the available/work
-					// resources i.e.free the resources
-					for (int k = 0 ; k < R ; k++)
+					}
+				}
+				//If all needs of p were satisfied.
+				if (j == R) {
+					//Add the allocated resources of current P to the available/work resources i.e.free the resources
+					for (int k = 0 ; k < R ; k++) {
 						work[k] += allot[p][k];
-
-					// Add this process to safe sequence.
+					}
+					//Add this process to safe sequence.
 					safeSeq[count++] = p;
-
-					// Mark this p as finished
 					finish[p] = 1;
-
 					found = true;
 				}
 			}
 		}
 
-		// If we could not find a next process in safe
-		// sequence.
-		if (found == false)
-		{
+		//If we could not find a next process in safe sequence.
+		if (found == false) {
 			cout << "System is not in safe state";
 			return false;
 		}
 	}
 
-	// If system is in safe state then
-	// safe sequence will be as below
-	cout << "System is in safe state.\nSafe"
-		" sequence is: ";
-	for (int i = 0; i < P ; i++)
+	//If system is in safe state then safe sequence will be as below
+	cout << "System is in safe state.\nSafe sequence is: ";
+	for (int i = 0; i < P ; i++) {
 		cout << safeSeq[i] << " ";
-
+	}
+	cout << endl;
 	return true;
 }
 
-// Driver code
+//Driver code
 int main()
 {
 	int processes[] = {0, 1, 2, 3, 4};
 
-	// Available instances of resources
-	int avail[] = {3, 3, 2};
+	std::ifstream file ("input.txt");
+    if(!file.is_open()) {
+        std::cout << "Unable to open file\n";
+    }
 
-	// Maximum R that can be allocated
-	// to processes
-	int maxm[][R] = {{7, 5, 3},
-					{3, 2, 2},
-					{9, 0, 2},
-					{2, 2, 2},
-					{4, 3, 3}};
+    while(!file.eof()) {
+		//Available instances of resources
+		//int avail[] = {3, 3, 2};
+		int avail[R];
+		for (int j = 0; j < R; j++) {
+  			file >> avail[j];
+		}
 
-	// Resources allocated to processes
-	int allot[][R] = {{0, 1, 0},
-					{2, 0, 0},
-					{3, 0, 2},
-					{2, 1, 1},
-					{0, 0, 2}};
+		int allot[P][R];
+		for (int i = 0; i < P; i++) {
+			for (int j = 0; j < R; j++) {
+  				file >> allot[i][j];
+			}
+		}
+		//Resources allocated to processes
+		// int allot[][R] = {{0, 1, 0},
+		// 				{2, 0, 0},
+		// 				{3, 0, 2},
+		// 				{2, 1, 1},
+		// 				{0, 0, 2}};
 
-	// Check system is in safe state or not
-	isSafe(processes, avail, maxm, allot);
+		int maxm[P][R];
+		for (int i = 0; i < P; i++) {
+			for (int j = 0; j < R; j++) {
+  				file >> maxm[i][j];
+			}
+		}
+		//Maximum R that can be allocated to processes
+		// int maxm[][R] = {{7, 5, 3},
+		// 				{3, 2, 2},
+		// 				{9, 0, 2},
+		// 				{2, 2, 2},
+		// 				{4, 3, 3}};
+		// }
 
+		//Check system is in safe state or not
+		isSafe(processes, avail, maxm, allot);
+	}
+    file.close();
 	return 0;
 }
